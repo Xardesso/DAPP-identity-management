c46cca5f3b2be9ab484b140f8151649bd7c70184
Xardesso <marcino53605@gmail.com>
2023-06-17 23:32:29 +0200

    major changes

--- identity-management-react/src/App.js
+++ identity-management-react/src/App.js
@@ -1,83 +1,58 @@
-import React from "react";
-import { useState } from "react";
-const ethers = require("ethers");
+import React, { useState } from "react";
+import "./style.css";
+export default function RegisterIdentity({ signer }) {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [nationality, setNationality] = useState();
+  const [height, setHeight] = useState();
 
-let provider;
-
-if (window.ethereum) {
-  console.log("check");
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const add = await provider.send("eth_requestAccounts", []);
-  console.log(add);
-  try {
-    // Request account access if needed
-    await window.ethereum.enable();
-  } catch (error) {
-    console.error(error);
-  }
-} else {
-  console.log(
-    "Non-Ethereum browser detected. You should consider trying MetaMask!"
-  );
-}
-const contractAddress = "0x90B07d58FbAfbd0B348D3021392631241382765e";
-const contractArtifacts = require("./artifacts/contracts/Identity.sol/IdentityManagement.json");
-const contractABI = contractArtifacts.abi;
-const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-const App = () => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [nationality, setNationality] = useState("");
-  const [height, setHeight] = useState("");
-
-  const handleRegister = async () => {
-    if (!provider) return;
-    const signer = provider.getSigner();
-    const contractWithSigner = contract.connect(signer);
-
-    try {
-      const tx = await contractWithSigner.registerIdentity(
-        name,
-        age,
-        nationality,
-        height
-      );
-      console.log("Transaction submitted: ", tx);
-    } catch (err) {
-      console.error("Error: ", err);
-    }
-  };
+  async function register() {}
+  window.location.href = "authorize.js";
 
   return (
-    <div>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
-      />
-      <input
-        type="text"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-        placeholder="Age"
-      />
-      <input
-        type="text"
-        value={nationality}
-        onChange={(e) => setNationality(e.target.value)}
-        placeholder="Nationality"
-      />
-      <input
-        type="text"
-        value={height}
-        onChange={(e) => setHeight(e.target.value)}
-        placeholder="Height"
-      />
-      <button onClick={handleRegister}>Register</button>
+    <div
+      className="main"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          height: "97px",
+          width: "1800pxpx",
+          textAlign: "center",
+        }}
+      >
+        <h1>Identify Yourself</h1>
+      </div>
+      <form
+        onSubmit={register}
+        style={{
+          textAlign: "center",
+          width: "200px",
+          height: "495px",
+          margin: "150px",
+        }}
+      >
+        <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <input placeholder="Age" onChange={(e) => setAge(e.target.value)} />
+        <input
+          placeholder="Nationality"
+          onChange={(e) => setNationality(e.target.value)}
+        />
+        <input
+          placeholder="Height"
+          onChange={(e) => setHeight(e.target.value)}
+        />
+        <button type="submit" style={{ marginTop: "10px" }}>
+          Register
+        </button>
+      </form>
+      <footer style={{ textAlign: "center", marginBottom: "20px" }}>
+        Marcin Letowski
+      </footer>
     </div>
   );
-};
-
-export default App;
+}
