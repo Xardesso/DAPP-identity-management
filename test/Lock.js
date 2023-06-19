cc0e5ec97a58f3735750f2482f144a60c959450d
Xardesso <marcino53605@gmail.com>
2023-06-19 03:15:56 +0200

    connect undefined

--- test/Lock.js
+++ test/Lock.js
@@ -1,126 +1,106 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("IdentityManagement", function () {
+  let IdentityManagement;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  let identityManagement;
+  let owner;
+  let authorizedEntity;
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+  beforeEach(async function () {
+    const identity = await ethers.deployContract("IdentityManagement");
+    [owner, authorizedEntity] = await ethers.getSigners();
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  it("should register an identity", async function () {
+    const name = "John Doe";
+    const age = "30";
+    const nationality = "USA";
+    const height = "180cm";
+    await identityManagement
+      .connect(owner)
+      .registerIdentity(name, age, nationality, height);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+    const values = {
+      gasLimit: 1000000,
+    };
+    const identity = await identityManagement.getIdentity(
+      owner.address,
+      values
+    );
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    expect(identity).to.deep.equal([name, age, nationality, height]);
+  });
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+  it("should grant authorization", async function () {
+    await identityManagement
+      .connect(owner)
+      .grantAuthorization(authorizedEntity.address);
+    const values = {
+      gasLimit: 1000000,
+    };
+    const isAuthorized = await identityManagement.showAuthorization(
+      authorizedEntity.address,
+      values
+    );
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
+    expect(isAuthorized).to.be.true;
+  });
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it("should revoke authorization", async function () {
+    const values = {
+      gasLimit: 1000000,
+    };
+    await identityManagement
+      .connect(owner)
+      .grantAuthorization(authorizedEntity.address, values);
+    await identityManagement
+      .connect(owner)
+      .revokeAuthorization(authorizedEntity.address, values);
 
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
+    const isAuthorized = await identityManagement.showAuthorization(
+      authorizedEntity.address,
+      values
+    );
 
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
+    expect(isAuthorized).to.be.false;
+  });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it("should get identity for authorized entity", async function () {
+    const name = "John Doe";
+    const age = "30";
+    const nationality = "USA";
+    const height = "180cm";
+    const values = {
+      gasLimit: 1000000,
+    };
+    await identityManagement
+      .connect(owner)
+      .registerIdentity(name, age, nationality, height, values);
+    await identityManagement
+      .connect(owner)
+      .grantAuthorization(authorizedEntity.address, values);
 
-        await time.increaseTo(unlockTime);
+    const identity = await identityManagement
+      .connect(authorizedEntity)
+      .getIdentity(owner.address, values);
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
+    expect(identity).to.deep.equal([name, age, nationality, height]);
+  });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  it("should fail to get identity for unauthorized entity", async function () {
+    const values = {
+      gasLimit: 1000000,
+    };
+    await identityManagement
+      .connect(owner)
+      .registerIdentity("John Doe", "30", "USA", "180cm", values);
 
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+    await expect(
+      identityManagement
+        .connect(authorizedEntity)
+        .getIdentity(owner.address, values)
+    ).to.be.revertedWith("Unauthorized entity");
   });
 });
